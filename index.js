2540af88ffbd75b0e59747f0d4f0604b8a9fa42c
elvis <gicharuelvis@gmail.com>
2022-08-17 07:15:38 +0300

    commit tree 1548c2dfba9962833ab9018d456e4c7e8195f800

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+// for (let age=30; age < 40; age++){
+//     console.log(`You are ${age} years old. Happy birthday to you`);
+    
+// };
+
+// const gifts=['teddy bear', 'drone','doll'];
+// function wrapGifts(gifts){
+//     for(let i=0;i < gifts.length; i++){
+//         console.log(`Wrapped ${gifts[i]} and added a bowl`);
+//         debugger;
+//     }
+//     return gifts;
+// }
+// wrapGifts();
+
+
+const names= ["Charlie", "Samip", "Ali"];
+const event='Birthday';
+function writeCards(names,event){
+    let thankyouCards =[]
+ for(let i=0; i < names.length ;i++){
+    thankyouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+ }
+
+ return thankyouCards;
+}
+
+let number= 15;
+function countDown(number){
+   while(number >-1){
+      console.log(number);
+      number --;
+   }
+}
+
